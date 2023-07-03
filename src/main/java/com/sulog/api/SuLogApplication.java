be2258e0d75aa59fd37ec5cce87630392c51af83
JongSooKim <prjs3643@gmail.com>
2023-07-03 21:55:01 +0900

    Initial commit

---
+++ src/main/java/com/sulog/api/SuLogApplication.java
@@ -0,0 +1,13 @@
+package com.sulog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuLogApplication.class, args);
+    }
+
+}
