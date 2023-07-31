bfb7609532f3f6b1de433a672f5a96c5302f6050
JongSooKim <prjs3643@gmail.com>
2023-07-31 21:44:54 +0900

    close #32 - Spring RestDocs index 파일 생성 - .adoc 파일 생성

--- src/test/java/com/sulog/api/docs/controller/RestDocPostControllerTest.java
+++ src/test/java/com/sulog/api/docs/controller/RestDocPostControllerTest.java
@@ -1,9 +1,12 @@
 package com.sulog.api.docs.controller;
 
+import com.sulog.api.domain.post.Post;
+import com.sulog.api.repository.post.PostRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
@@ -11,6 +14,7 @@ import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -20,6 +24,10 @@ import org.springframework.web.context.WebApplicationContext;
 public class RestDocPostControllerTest {
 
     private MockMvc mockMvc;
+
+    @Autowired
+    private PostRepository postRepository;
+
     @BeforeEach
     void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
@@ -33,5 +41,21 @@ public class RestDocPostControllerTest {
         this.mockMvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andDo(MockMvcRestDocumentation.document("index"));
+    }
+
+    @Test
+    @DisplayName("글 단건 테스트")
+    public void 글_단건_테스트() throws Exception {
+        Post post = Post.builder()
+                .title("12312312319876543")
+                .content("삼겹살 먹고 싶다.")
+                .build();
+
+        postRepository.save(post);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/posts/{postId}", post.getId()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcRestDocumentation.document("index"))
+                .andDo(MockMvcResultHandlers.print());
     }
 }
