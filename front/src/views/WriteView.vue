ba0719e16949f3e968b500310a419a8f1e2ed0aa
KimJongSoo <prjs3643@gmail.com>
2023-11-23 23:25:06 +0900

    Vue 사용

--- front/src/views/WriteView.vue
+++ front/src/views/WriteView.vue
@@ -1,37 +1,40 @@
 <script setup lang="ts">
-import {ref} from "vue";
-import axios from "axios";
-import {useRouter} from "vue-router";
+import { ref } from "vue";
 
-const count = ref(0)
-const title = ref("")
-const content = ref("")
+import axios from "axios";
+import { useRouter } from "vue-router";
+
+const title = ref("");
+const content = ref("");
 
 const router = useRouter();
-const write = function() {
-  axios.post("/api/posts", {
-    title: title.value,
-    content: content.value
-  }).then(() => {
-    router.replace({name:"home"});
-  });
-}
+
+const write = function () {
+  axios
+      .post("/api/posts", {
+        title: title.value,
+        content: content.value,
+      })
+      .then(() => {
+        router.replace({ name: "home" });
+      });
+};
 </script>
 
 <template>
   <div>
-    <el-input type="text" v-model="title" placeholder="제목을 입력해주세요."/>
-  </div>
-
-  <div>
-    <el-input type="textarea" v-model="content" rows="15"></el-input>
+    <el-input v-model="title" placeholder="제목을 입력해주세요" />
   </div>
 
   <div class="mt-2">
-    <el-button type="primary" @click="write()">글 작성완료</el-button>
+    <el-input v-model="content" type="textarea" rows="15" />
+  </div>
+
+  <div class="mt-2">
+    <div class="d-flex justify-content-end">
+      <el-button type="primary" @click="write()">작성완료</el-button>
+    </div>
   </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
